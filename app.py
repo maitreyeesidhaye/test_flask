08ade59d86a24216db418b05e9be6e61d9c89dcf
maitreyeesidhaye <mai.sidhaye@mail.utoronto.ca>
2020-11-29 12:31:36 -0500

    sqlite app

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import sqlite3
-from flask import Flask  # import flask
+
+# import the Flask class from the flask module
+from flask import Flask, render_template, redirect, url_for, request
 
 app = Flask(__name__)  # create an app instance
 
@@ -21,15 +23,49 @@ def insert_user(name, age):
     # Inserts new record in db.collection (data must be in JSON)
     connie = sqlite3.connect(URI)
     c1 = connie.cursor()
-    c1.execute("INSERT INTO users (name, age) VALUES(?, ?)",
-              (name, age))
+    c1.execute("INSERT INTO users_test (name, age) VALUES(?, ?)",
+               (name, age))
     connie.commit()
     connie.close()
 
 
-@app.route("/")  # at the end point /
-def hello():  # call method hello
-    return "Hello World!"  # which returns "hello world"
+def get_user_login_data(name):
+    """ Returns user login based on email in the dictionary format """
+
+    conn1 = sqlite3.connect(URI)
+    c2 = conn1.cursor()
+    col_cursor = c2.execute('select * from users_test')
+    user_table_col_names = list(map(lambda x: x[0], col_cursor.description))
+    c2.execute("SELECT * FROM users_test WHERE name = ?", (name,))
+    data = c2.fetchall()
+    conn1.close()
+    user_dic = {}
+    for i in range(len(data[0])):
+        user_dic[user_table_col_names[i]] = data[0][i]
+    return user_dic
+
+
+# use decorators to link the function to a url
+@app.route('/')
+def home():
+    return "Hello, Girl!"  # return a string
+
+
+@app.route('/welcome')
+def welcome():
+    return render_template('welcome.html')  # render a template
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        name = request.form['name']
+        age = request.form['age']
+        print(name, age)
+        insert_user(name, age)
+        xx = get_user_login_data(name)
+        print(xx)
+    return render_template('login.html')
 
 
 if __name__ == "__main__":  # on running python app.py
